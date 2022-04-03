2015969f04041a2fa349bdfdcf76afd4f16aff48
rinat.idiiatov <rinat.idiiatov@qlever.com>
2022-04-03 11:43:10 +0300

    deploy

---
+++ precache-manifest.6931d255bba8057f3bc300495e720995.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe5f13460c86e438f3df",
+    "url": "/project-resume/css/app.b1403dfb.css"
+  },
+  {
+    "revision": "954b2e3105d6151adf1b",
+    "url": "/project-resume/css/chunk-vendors.fa0c18c4.css"
+  },
+  {
+    "revision": "6b3d1fea378c787df401ab87a9163bcf",
+    "url": "/project-resume/fonts/NotoSansSC_Bold.6b3d1fea.woff"
+  },
+  {
+    "revision": "bbff225a55e99a87e13640f5ec037b63",
+    "url": "/project-resume/fonts/NotoSansSC_Bold.bbff225a.eot"
+  },
+  {
+    "revision": "bcd273eab09ebdc4af7e88366dbbe0a7",
+    "url": "/project-resume/fonts/NotoSansSC_Bold.bcd273ea.ttf"
+  },
+  {
+    "revision": "2f950391d61953b6ceaff7f2d1a5c997",
+    "url": "/project-resume/fonts/NotoSansSC_Medium.2f950391.ttf"
+  },
+  {
+    "revision": "5c2e56568f0f26fc91467681c58dd4bf",
+    "url": "/project-resume/fonts/NotoSansSC_Medium.5c2e5656.woff"
+  },
+  {
+    "revision": "b4e5761346c8924f351063c4b2ae0d6b",
+    "url": "/project-resume/fonts/NotoSansSC_Medium.b4e57613.eot"
+  },
+  {
+    "revision": "065866396c04386cc8afeddfe54ab151",
+    "url": "/project-resume/fonts/NotoSansSC_Regular.06586639.woff"
+  },
+  {
+    "revision": "85f91329e79c3db3c43e5b7756f1fc8e",
+    "url": "/project-resume/fonts/NotoSansSC_Regular.85f91329.ttf"
+  },
+  {
+    "revision": "dd9f000c97c75048fed5406b06378426",
+    "url": "/project-resume/fonts/NotoSansSC_Regular.dd9f000c.eot"
+  },
+  {
+    "revision": "0545fac61520f6c9604ec3f02e86a662",
+    "url": "/project-resume/fonts/SF_Pro_Display_Regular.0545fac6.woff"
+  },
+  {
+    "revision": "c9d4c1c99f87b2da004d78948c3835bf",
+    "url": "/project-resume/fonts/SF_Pro_Display_Regular.c9d4c1c9.eot"
+  },
+  {
+    "revision": "d09549c1ab4a5947a007561521e45da3",
+    "url": "/project-resume/fonts/SF_Pro_Display_Regular.d09549c1.ttf"
+  },
+  {
+    "revision": "fa41603037af668904a36f60ad4dbc8c",
+    "url": "/project-resume/img/NotoSansSC_Bold.fa416030.svg"
+  },
+  {
+    "revision": "f8fdb7c4d478bc6520774efb499d1c9b",
+    "url": "/project-resume/img/NotoSansSC_Medium.f8fdb7c4.svg"
+  },
+  {
+    "revision": "967d99a8d4e5861b4059b4ee2a8b74e0",
+    "url": "/project-resume/img/NotoSansSC_Regular.967d99a8.svg"
+  },
+  {
+    "revision": "b4b7398800b76f2488abf9841af7a6a8",
+    "url": "/project-resume/img/SF_Pro_Display_Regular.b4b73988.svg"
+  },
+  {
+    "revision": "fb1f696ea9f2c47fed8487057025cf3c",
+    "url": "/project-resume/index.html"
+  },
+  {
+    "revision": "fe5f13460c86e438f3df",
+    "url": "/project-resume/js/app.e9b3c686.js"
+  },
+  {
+    "revision": "954b2e3105d6151adf1b",
+    "url": "/project-resume/js/chunk-vendors.2c6c463c.js"
+  },
+  {
+    "revision": "f4e9035df925ba7f52a8ea5f4cfc455a",
+    "url": "/project-resume/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/project-resume/robots.txt"
+  }
+]);
